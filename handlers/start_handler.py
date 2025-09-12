ee59b441514f3d5ace8627fadc0d3887883a9e88
Вера Королева <verakoroleva@MacBook-Air-Vera.local>
2025-09-13 02:59:58 +0500

    feat: change 'Authorize' button to inline button and update documentation

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,6 +1,9 @@
-from telegram import Update
+import os
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from telegram.helpers import escape_markdown
+
+WEB_APP_URL = os.getenv("WEB_APP_AUTH_URL")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Отправляет приветственное сообщение при команде /start."""
@@ -8,4 +11,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     name = escape_markdown(user.first_name or "пользователь")
     welcome_message = f"Добрый день, {name}, для продолжения вам необходимо авторизоваться"
 
-    await update.message.reply_text(welcome_message)
+    keyboard = [
+        [InlineKeyboardButton("Авторизоваться", web_app={"url": WEB_APP_URL})]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(welcome_message, reply_markup=reply_markup)
